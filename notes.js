ab4587edfb3c2b95a3742629b46e527700aaa9fa
Chandrashekhar Singh Mourya <118621709+Singh-csm@users.noreply.github.com>
2023-02-27 20:16:53 +0530

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,11 @@
+https://cs.slides.com/colt_steele/built-in-data-structures-30#/36/0/2
+
+https://cs.slides.com/colt_steele/es2015-class-syntax#/32/0/0
+
+https://cs.slides.com/colt_steele/problem-solving-patterns#/36
+
+
+https://exploringjs.com/es6/ch_variables.html#sec_let-const-loop-heads
+
+
+css: https://getcssscan.com/css-box-shadow-examples
